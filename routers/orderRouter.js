58277f89a5d2d782f638d0dd81f72c91f8384cb7
Swechhya Maharjan <swekchya.mjn@gmail.com>
2025-08-31 22:17:52 +0545

    Added myorder, isPaid, isDelivered function

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,10 +1,15 @@
 import express from "express"
-import { addOrder, getOrders } from "../controllers/orderController.js";
+import { addOrder, deliverOrder, getMyOrders, getOrderById, getOrders, payOrder } from "../controllers/orderController.js";
 import checkAuth from "../middlewares/checkAuth.js";
 import checkAdmin from "../middlewares/checkAdmin.js";
 
 const router = express.Router();
 
 router.post("/" ,checkAuth, addOrder);
-router.get("/", checkAuth, checkAdmin, getOrders );
+router.get("/", checkAuth, checkAdmin, getOrders);
+router.get("/myorder", checkAuth, getMyOrders);
+router.get("/:id", checkAuth, getOrderById);
+router.put("/pay/:id", checkAuth, checkAdmin, payOrder);
+router.put("/deliver/:id", checkAuth, checkAdmin, deliverOrder);
+
 export default router;
